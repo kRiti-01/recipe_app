6b6acb5298fde51ee4746708d2ad559a55d7958d
Kriti Gupta <kritigupta7031@gmail.com>
2023-05-27 18:31:40 +0530

    next

--- script.js
+++ script.js
@@ -34,3 +34,56 @@ async function getMealsBySearch(term) {
     const resp = await fetch(
         "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
     );
+    const respData = await resp.json();
+    const meals = respData.meals;
+
+    return meals;
+}
+
+function addMeal(mealData, random = false) {
+    console.log(mealData);
+
+    const meal = document.createElement("div");
+    meal.classList.add("meal");
+
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${
+                random
+                    ? `
+            <span class="random"> Random Recipe </span>`
+                    : ""
+            }
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `;
+
+    const btn = meal.querySelector(".meal-body .fav-btn");
+
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("active")) {
+            removeMealLS(mealData.idMeal);
+            btn.classList.remove("active");
+        } else {
+            addMealLS(mealData.idMeal);
+            btn.classList.add("active");
+        }
+
+        fetchFavMeals();
+    });
+
+    meal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
+    mealsEl.appendChild(meal);
+}
