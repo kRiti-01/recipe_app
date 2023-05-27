fc7a93e14ce8cbf71589feb3cb349e337e516031
Kriti Gupta <kritigupta7031@gmail.com>
2023-05-27 18:33:15 +0530

    next

--- script.js
+++ script.js
@@ -87,3 +87,23 @@ function addMeal(mealData, random = false) {
 
     mealsEl.appendChild(meal);
 }
+function addMealLS(mealId) {
+    const mealIds = getMealsLS();
+
+    localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+}
+
+function removeMealLS(mealId) {
+    const mealIds = getMealsLS();
+
+    localStorage.setItem(
+        "mealIds",
+        JSON.stringify(mealIds.filter((id) => id !== mealId))
+    );
+}
+
+function getMealsLS() {
+    const mealIds = JSON.parse(localStorage.getItem("mealIds"));
+
+    return mealIds === null ? [] : mealIds;
+}
